6694f46625d9fa122114f72471d71add261ce0d8
Zachery2008 <46687719+Zachery2008@users.noreply.github.com>
2019-12-05 10:23:28 -0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world Zzz" );
+    res.send( "Hello world Zzz from port: " + port);
 } );
 
 // start the Express server
